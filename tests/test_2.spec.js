370ddf8db95d5c058299a72093c7f8bb3d378f82
Meghana Katragadda <meghanak9948@gmail.com>
2023-10-27 07:12:58 +0000

    commit

--- tests/test_2.spec.js
+++ tests/test_2.spec.js
@@ -2,67 +2,19 @@ const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
 test.describe('Browse LambdaTest in different search engines 2', () => {
-  test('Search LambdaTest on Bing', async ({ page }) => {
-    await page.setViewportSize({ width: 1920, height: 1080 });
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-    await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
-    await page.click('text=Get Started');
-    await expect(page.locator('text=Installation').first()).toBeVisible();
-    await page.click('text=Trace Viewer');
-    await page.click('text=Test Generator');
-    await page.click('text=Release notes');
-    await page.click('text=Annotations');
-    await page.click('text=API testing');
-    await page.click('text=Authentication');
-    await page.click('text=Command line');
-    await page.click('text=Configuration');
-    await page.click('text=Page Object Model');
-    await page.click('text=Parameterize tests');
-    await page.click('text=Reporters');
-    await page.click('text=Retries');
-    await page.click('text=Timeouts');
-    await page.click('text=Visual comparisons');
-    await page.click('text=Fixtures');
-    await page.click('text=TypeScript');
-    await page.click('text=Experimental: components');
-    await page.click('text=Library');
-    await page.click('text=Auto-waiting');
-    await page.click('text=Authentication');
-    await page.click('text=Browsers');
-    await page.click('text=Chrome Extensions');
-    await page.click('text=Command line');
-    await page.click('text=Dialogs');
-    await page.click('text=Downloads');
-    await page.click('text=Emulation');
-    await page.click('text=Evaluating JavaScript');
-    await page.click('text=Events');
-    await page.click('text=Extensibility');
-    await page.click('text=Frames');
-    await page.click('text=Handles');
-    await page.click('text=Locators');
-    await page.click('text=Navigations');
-    await page.click('text=Network');
-    await page.click('text=Pages');
-    await page.click('text=Page Object Models');
-    await page.click('text=Screenshots');
-    await page.click('text=Videos');
-    await page.click('text=Migration');
-    await page.click('text=Migrating from Protractor');
-    await page.click('text=Integrations');
-    await page.click('text=Docker');
-    await page.click('text=Continuous Integration');
-    await page.click('text=Selenium Grid');
-    await page.click('text=Supported languages');
-    await page.goto('https://www.bing.com')
-    const element = await page.$('[id="sb_form_q"]')
-    await element.click()
-    await element.type('LambdaTest')
-    await element.press('Enter')
-    const title = await page.title()
-
-    console.log('Page title:: ', title)
-    // Use the expect API for assertions provided by playwright
-    expect(title).toEqual(expect.stringContaining('LambdaTest'))
+  test('Test_2', async ({ page }) => {
+    await page.goto("https://www.lambdatest.com/selenium-playground/")
+    await page.click("//a[text()='Drag & Drop Sliders']")
+    const slider=await page.locator("//input[@type='range' and @value='15']")
+    let temp =15
+       while(temp!='95'){
+               let expval =await page.locator("//output[@id='rangeSuccess']").getByText()
+                await slider.press('ArrowRight')
+            temp++
+             }
+             console.log(temp)
+        await page.waitForTimeout(5000)
+    
+    })
   })
-})
+
--- tests/test_3.spec.js
+++ tests/test_3.spec.js
@@ -2,67 +2,34 @@ const { test } = require('../lambdatest-setup')
 const { expect } = require('@playwright/test')
 
 test.describe('Browse LambdaTest in different search engines 3', () => {
-  test('Search LambdaTest on Bing', async ({ page }) => {
+  test('Test_3', async ({ page }) => {
     await page.setViewportSize({ width: 1920, height: 1080 });
-    await page.goto('https://playwright.dev/');
-    await expect(page).toHaveTitle(/Playwright/);
-    await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
-    await page.click('text=Get Started');
-    await expect(page.locator('text=Installation').first()).toBeVisible();
-    await page.click('text=Trace Viewer');
-    await page.click('text=Test Generator');
-    await page.click('text=Release notes');
-    await page.click('text=Annotations');
-    await page.click('text=API testing');
-    await page.click('text=Authentication');
-    await page.click('text=Command line');
-    await page.click('text=Configuration');
-    await page.click('text=Page Object Model');
-    await page.click('text=Parameterize tests');
-    await page.click('text=Reporters');
-    await page.click('text=Retries');
-    await page.click('text=Timeouts');
-    await page.click('text=Visual comparisons');
-    await page.click('text=Fixtures');
-    await page.click('text=TypeScript');
-    await page.click('text=Experimental: components');
-    await page.click('text=Library');
-    await page.click('text=Auto-waiting');
-    await page.click('text=Authentication');
-    await page.click('text=Browsers');
-    await page.click('text=Chrome Extensions');
-    await page.click('text=Command line');
-    await page.click('text=Dialogs');
-    await page.click('text=Downloads');
-    await page.click('text=Emulation');
-    await page.click('text=Evaluating JavaScript');
-    await page.click('text=Events');
-    await page.click('text=Extensibility');
-    await page.click('text=Frames');
-    await page.click('text=Handles');
-    await page.click('text=Locators');
-    await page.click('text=Navigations');
-    await page.click('text=Network');
-    await page.click('text=Pages');
-    await page.click('text=Page Object Models');
-    await page.click('text=Screenshots');
-    await page.click('text=Videos');
-    await page.click('text=Migration');
-    await page.click('text=Migrating from Protractor');
-    await page.click('text=Integrations');
-    await page.click('text=Docker');
-    await page.click('text=Continuous Integration');
-    await page.click('text=Selenium Grid');
-    await page.click('text=Supported languages');
-    await page.goto('https://www.bing.com')
-    const element = await page.$('[id="sb_form_q"]')
-    await element.click()
-    await element.type('LambdaTest')
-    await element.press('Enter')
-    const title = await page.title()
+    await page.goto("https://www.lambdatest.com/selenium-playground/input-form-demo")
+const msg='Please fill in the fields.'
+ const a= await page.locator("//button[text()='Submit']").click()
 
-    console.log('Page title:: ', title)
-    // Use the expect API for assertions provided by playwright
-    expect(title).toEqual(expect.stringContaining('LambdaTest'))
+await page.locator("//input[@id='name']").fill("Meghana")
+await page.locator("//input[@id='inputEmail4']").fill("meghanakatragadda99@gmail.com")
+await page.locator("//input[@id='inputPassword4']").fill("Meghana@1234")
+
+await page.locator("//input[@id='company']").fill("ABC")
+await page.locator("//input[@name='website']").fill("ABC.com")
+await page.locator("//select[@name='country']").selectOption('United States')
+await page.locator("//input[@name='city']").fill("NY")
+await page.locator("//input[@name='address_line1']").fill("xyz")
+await page.locator("//input[@name='address_line2']").fill("ghy")
+await page.locator("//input[@placeholder='State']").fill("New York")
+await page.locator("//input[@name='zip']").fill("37700")
+await page.click("//button[text()='Submit']")
+const sucmes='Thanks for contacting us, we will get back to you shortly.'
+const messag=await page.locator("//p[@style='display: block;']").innerText(sucmes)
+
+if(sucmes==messag){
+    console.log(sucmes)
+}
+else{
+    console.log('not success')
+}
+await page.waitForTimeout(10000)
   })
 })
